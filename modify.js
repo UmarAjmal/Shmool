50f1734af4b415380325d2248f16c025a48370c1
Muhammad Umar Ajmal <126502013+UmarAjmal@users.noreply.github.com>
2026-04-09 09:19:14 +0500

    Update

--- modify.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('client/components/dashboards/StudentDashboard.tsx', 'utf8');
-
-exports = { content, write: () => fs.writeFileSync('client/components/dashboards/StudentDashboard.tsx', content) };
